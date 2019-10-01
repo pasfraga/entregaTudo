41bd2179b4aba4ac80e901b66ceb2b9d3e2aeb22
Técnico em Informática <24121742018.1@senacrjedu.local>
2019-09-30 21:39:53 -0300

    aula03

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UsuarioService } from '../services/usuario.service';
 
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -9,11 +10,18 @@ import { UsuarioService } from '../services/usuario.service';
 export class Tab2Page {
 
   protected usuarios: any;
-
-  constructor(
+  constructor( 
     protected usuarioService:UsuarioService
   ) {
     this.usuarios = this.usuarioService.getAll();
   }
+  async doRefresh(event) {
+    console.log('Begin async operation');
+    this.usuarios = await this.usuarioService.getAll()
 
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 500);
+  }
 }
